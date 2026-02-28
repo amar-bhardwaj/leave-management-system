e586907bfea00d2db6c5a5da5c2ff1fe5c4da659
amar-bhardwaj <amar.sharma295@gmail.com>
2026-02-28 19:42:09 +0530

    added root route

--- backend/server.js
+++ backend/server.js
@@ -26,4 +26,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
 app.listen(PORT, () => console.log("Server running"));
