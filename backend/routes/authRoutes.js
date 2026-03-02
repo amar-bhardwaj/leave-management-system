82dfaaf00a8a3386d8e22cbc7f0f663e3c2469c9
amar-bhardwaj <amar.sharma295@gmail.com>
2026-03-02 13:53:31 +0530

    forced employee role

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,17 +3,15 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-// Register
+//Register
 router.post("/register", async (req, res) => {
-  const { name, email, password, role } = req.body;
-
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const { name, email, password } = req.body;
 
   const user = await User.create({
     name,
     email,
-    password: hashedPassword,
-    role
+    password,
+    role: "employee" // 🔒 force role
   });
 
   res.json(user);
