657326fd7d5ea5ce14d53f35acb4662dff2a663b
amar-bhardwaj <amar.sharma295@gmail.com>
2026-03-02 18:54:57 +0530

    clean restart

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
 
@@ -27,7 +28,7 @@ router.post(
   }
 );
 
-const bcrypt = require("bcryptjs");
+
 
 // TEMP ROUTE - CREATE FIRST ADMIN
 router.get("/create-admin", async (req, res) => {
@@ -42,6 +43,40 @@ router.get("/create-admin", async (req, res) => {
     });
 
     res.json(admin);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// LOGIN ROUTE
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // create token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.json({
+      token,
+      role: user.role
+    });
+
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
