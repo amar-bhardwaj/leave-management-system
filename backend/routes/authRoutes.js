d8bc1bfa9e54fe3f3b545b4657a7da19d41cd50e
amar-bhardwaj <amar.sharma295@gmail.com>
2026-03-02 17:23:28 +0530

    create-admin-route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -13,15 +13,38 @@ router.post(
   async (req, res) => {
     const { name, email, password } = req.body;
 
+    // 🔥 HASH PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       role: "employee"
     });
 
     res.json(user);
   }
 );
+
+const bcrypt = require("bcryptjs");
+
+// TEMP ROUTE - CREATE FIRST ADMIN
+router.get("/create-admin", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+
+    const admin = await User.create({
+      name: "Main Admin",
+      email: "admin@company.com",
+      password: hashedPassword,
+      role: "admin"
+    });
+
+    res.json(admin);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 module.exports = router;
